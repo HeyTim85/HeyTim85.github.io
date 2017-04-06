9cc6c7bd39c0e787cfc17989fdcee0282da4a73d
HeyTim85 <Timothy85@missouristate.edu>
2017-04-05 19:22:29 -0500

    Clock in

---
+++ myclock.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+<!-- myclockscript -->
+dy = new Date()
+month = dy.getMonth()
+month = (month * 1) + 1
+day = dy.getDate()
+year = dy.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
